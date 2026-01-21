b2a1a6a0e738381b4a23e4bcf8c3dc040b4abcf3
Gaurav Rathod <gauravrth012@gmail.com>
2026-01-21 22:44:33 +0530

    Delete src/main/java/com/skyfleet/rentals directory

--- src/main/java/com/skyfleet/rentals/custom_exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.skyfleet.rentals.custom_exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-	public ResourceNotFoundException(String message) {
-		super(message);
-	}
-}
